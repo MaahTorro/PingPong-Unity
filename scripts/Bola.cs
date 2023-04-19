1c39a532f2a924f553506d96ba33d73431dafe55
Marcela Torro <99346289+MaahTorro@users.noreply.github.com>
2023-04-19 11:25:28 -0300

    Add files via upload

---
+++ scripts/Bola.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+
+    public float velocidadeDaBola;
+    public Rigidbody2D oRigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oRigidbody2D.velocity = new Vector2(velocidadeDaBola, velocidadeDaBola ) *Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
